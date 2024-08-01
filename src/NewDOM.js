87d07cf1e483f8a1e62c3a8a91202e964fb60fb4
Sahana Deepak <sahanad149@gmail.com>
2024-08-01 16:10:30 -0700

    final commit

--- src/NewDOM.js
+++ src/NewDOM.js
@@ -10,23 +10,58 @@ let reconciler = ReactReconciler({
         rootContainerInstance,
         hostContext,
         internalInstanceHandle,
-    ) {},
+    ) {
+        // console.log(type, props);
+        let el = document.createElement(type);
+        if (props.className) el.className = props.className;
+        if (props.src) el.src = props.src;
+        ['alt', 'className', 'href', 'rel', 'src', 'target'].forEach(k => {
+            if (props[k]) el[k] = props[k];
+        });
+
+        if(props.onClick) {
+            el.addEventListner('click', props.onClick);
+        }
+
+        if (props.bgColor) {
+            el.style.backgroundColor = props.bgColor;
+        }
+
+        return el;
+    },
 
     createTextInstance(
         text,
         rootContainerInstance,
         hostContext,
         internalInstanceHandle,
-    ) {},
+    ) {
+        // <div>Click Here <button/></div>
+        return document.createTextNode(text);
+    },
 
-    // appendChildToContainer(container, child) {},
-    // appendChild(parent, child) {},
-    // appendInitialChild(parent, child) {},
+    appendChildToContainer(container, child) {
+        container.appendChild(child);
+    },
+    appendChild(parent, child) {
+        parent.appendChild(child);
+    },
+    appendInitialChild(parent, child) {
+        parent.appendChild(child);
+    },
 
-    // removeChildFromContainer(container, child){},
-    // removeChild(parent, child) {},
-    // insertInContainerBefore(container, child, before) {},
-    // insertBefore(parent, child, before) {},
+    removeChildFromContainer(container, child){
+        container.removeChild(child);
+    },
+    removeChild(parent, child) {
+        parent.removeChild(child);
+    },
+    insertInContainerBefore(container, child, before) {
+        container.insertBefore(child, before);
+    },
+    insertBefore(parent, child, before) {
+        parent.insertBefore(child, before);
+    },
 
     prepareUpdate(
         instance, 
@@ -35,7 +70,13 @@ let reconciler = ReactReconciler({
         newProps,
         rootContainerInstance,
         currentHostContext,
-    ) {},
+    ) {
+        let payload;
+        if (oldProps.bgColor !== newProps.bgColor) {
+            payload = {newBgColor: newProps.bgColor};
+        }
+        return payload;
+    },
 
     commitUpdate(
         instance,
@@ -44,7 +85,11 @@ let reconciler = ReactReconciler({
         oldProps,
         newProps,
         finishedWork,
-    ) {},
+    ) {
+        if (updatePayload.newBgColor) {
+            instance.style.backgroundColor = updatePayload.newBgColor
+        }
+    },
 
     finalizeInitialChildren() {},
     getChildHostContext() {},
